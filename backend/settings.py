8b4bb40375486b307d82cc5f406ad3b1368e4456
Sushant Gaikwad <sushantgaikwad2803@gmail.com>
2026-01-11 19:59:28 +0530

    Update CORS for new frontend and localhost

--- backend/settings.py
+++ backend/settings.py
@@ -100,25 +100,25 @@ CSRF_TRUSTED_ORIGINS = [
 # DATABASE (RENDER)
 # =====================
 
-# DATABASES = {
-#     "default": dj_database_url.parse(
-#         os.environ.get("DATABASE_URL"),
-#         conn_max_age=600,
-#         ssl_require=True,
-#     )
-# }
-
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "12345",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "postgres",
+#         "USER": "postgres",
+#         "PASSWORD": "12345",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 
 # =====================
 # CLOUDINARY
